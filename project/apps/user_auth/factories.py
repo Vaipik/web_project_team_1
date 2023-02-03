25971e8801750dd9c688474c29bf3e56aede4fad
Vaipik <n.khylko@gmail.com>
2023-02-03 18:07:13 +0200

    user_auth factories

--- project/apps/user_auth/factories.py
+++ project/apps/user_auth/factories.py
@@ -7,6 +7,7 @@ User = get_user_model()
 class UserFactory(factory.django.DjangoModelFactory):
 
     username = factory.Sequence(lambda n: f"test_username_{n}")
+    password = factory.PostGenerationMethodCall('set_password', 'password')
 
     class Meta:
         model = User

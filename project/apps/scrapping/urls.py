13ffd84a079519558fd470979c2c21717d8e75b3
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2023-02-04 13:39:35 +0200

    Add scrapping urls

---
+++ project/apps/scrapping/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('news/', views.get_news, name='news'),
+    path('sport_news/', views.get_sport_news, name='sport_news'),
+    path('tech_news/', views.get_tech_news, name='tech_news'),
+    path('python_books/', views.get_python_books, name='python_books'),
+    path('currency/', views.get_currency, name='currency'),
+]

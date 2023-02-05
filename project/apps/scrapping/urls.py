fadcfa6e96ccf3afa2519d169a925037d9449772
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2023-02-05 19:51:15 +0200

    Added app_name

--- project/apps/scrapping/urls.py
+++ project/apps/scrapping/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'scrapping'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('news/', views.get_news, name='news'),

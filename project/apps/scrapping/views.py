d8712237f009f3f885d541cafca5d2ddfe793987
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2023-02-07 15:41:19 +0200

    Added paginate

--- project/apps/scrapping/views.py
+++ project/apps/scrapping/views.py
@@ -1,39 +1,51 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .services.scrapy_news import scrapy_ukr_news, scrapy_sport_news, scrapy_tech_news, scrapy_python_books, \
     scrapy_currency
+from .services.paginator import get_paginator
 
 
 # Create your views here.
+# @login_required
 def index(request):
     return render(request, 'scrapping/index.html')
 
 
+# @login_required
 def get_news(request):
     news = scrapy_ukr_news()
-    return render(request, 'scrapping/scrape_news.html', {'news': news})
+    page_obj = get_paginator(request, news, 10)
+    return render(request, 'scrapping/scrape_news.html', {'page_obj': page_obj})
 
 
+# @login_required
 def get_sport_news(request):
     sport_news = scrapy_sport_news()
-    return render(request, 'scrapping/scrape_sport_news.html', {'sport_news': sport_news})
+    page_obj = get_paginator(request, sport_news, 10)
+    return render(request, 'scrapping/scrape_sport_news.html', {'page_obj': page_obj})
 
 
+# @login_required
 def get_tech_news(request):
     tech_news = scrapy_tech_news()
-    return render(request, 'scrapping/scrape_tech_news.html', {'tech_news': tech_news})
+    page_obj = get_paginator(request, tech_news, 10)
+    return render(request, 'scrapping/scrape_tech_news.html', {'page_obj': page_obj})
 
 
+# @login_required
 def get_python_books(request):
     python_books = scrapy_python_books()
-    return render(request, 'scrapping/scrape_python_books.html', {'python_books': python_books})
+    page_obj = get_paginator(request, python_books, 10)
+    return render(request, 'scrapping/scrape_python_books.html', {'page_obj': page_obj})
 
 
+# @login_required
 def get_currency(request):
     if request.method == 'POST':
         currency = request.POST.get('currency')
         date = request.POST.get('date')
         currency_list = scrapy_currency(currency, date)
-        return render(request, 'scrapping/scrape_currency.html',
-                      {'currency_list': currency_list, 'currency': currency, 'date': date})
+        return render(request, 'scrapping/scrape_currency.html', {'currency_list': currency_list})
     return render(request, 'scrapping/scrape_currency.html')

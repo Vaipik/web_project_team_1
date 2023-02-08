9649c333d796a08bce7982bf5ffcde93ec9da6ef
Alexey Vedmedev <al.vedmedev@gmail.com>
2023-02-08 20:15:41 +0200

    some fix

--- project/apps/search_app/urls.py
+++ project/apps/search_app/urls.py
@@ -7,5 +7,6 @@ app_name = "search_app"
 urlpatterns = [
     re_path(r"^files.+search$", SearchFileView.as_view(), name="search_files"),
     re_path(r"^contacts.+search$", SearchContactView.as_view(), name="search_contacts"),
-    re_path(r"^.*search$", SearchUniView.as_view(), name="search_results"),  """this path must be at the end"""
+    # """this path must be at the end"""
+    re_path(r"^.*search$", SearchUniView.as_view(), name="search_results"),
 ]

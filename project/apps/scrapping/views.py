e5c4d4a429e73e2acdec6a629fd77df560deb228
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2023-02-09 14:36:13 +0200

    added page_range for paginate, unlock @login_required

--- project/apps/scrapping/views.py
+++ project/apps/scrapping/views.py
@@ -1,46 +1,46 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .services.paginator import get_paginator
 from .services.scrapy_news import scrapy_ukr_news, scrapy_sport_news, scrapy_tech_news, scrapy_python_books, \
     scrapy_currency
-from .services.paginator import get_paginator
 
 
 # Create your views here.
-# @login_required
+@login_required
 def index(request):
     return render(request, 'scrapping/index.html')
 
 
-# @login_required
+@login_required
 def get_news(request):
     news = scrapy_ukr_news()
-    page_obj = get_paginator(request, news, 10)
+    page_obj, page_range = get_paginator(request, news, 10)
     return render(request, 'scrapping/scrape_news.html', {'page_obj': page_obj})
 
 
-# @login_required
+@login_required
 def get_sport_news(request):
     sport_news = scrapy_sport_news()
-    page_obj = get_paginator(request, sport_news, 10)
+    page_obj, page_range = get_paginator(request, sport_news, 10)
     return render(request, 'scrapping/scrape_sport_news.html', {'page_obj': page_obj})
 
 
-# @login_required
+@login_required
 def get_tech_news(request):
     tech_news = scrapy_tech_news()
-    page_obj = get_paginator(request, tech_news, 10)
+    page_obj, page_range = get_paginator(request, tech_news, 10)
     return render(request, 'scrapping/scrape_tech_news.html', {'page_obj': page_obj})
 
 
-# @login_required
+@login_required
 def get_python_books(request):
     python_books = scrapy_python_books()
-    page_obj = get_paginator(request, python_books, 8)
+    page_obj, page_range = get_paginator(request, python_books, 8)
     return render(request, 'scrapping/scrape_python_books.html', {'page_obj': page_obj})
 
 
-# @login_required
+@login_required
 def get_currency(request):
     if request.method == 'POST':
         currency = request.POST.get('currency')

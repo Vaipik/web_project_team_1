ca5e440d7f5fb3541a79c4b79f912e19d898f879
Yanina Lubenska <yaninaosadchaya@gmail.com>
2023-02-10 16:15:26 +0200

    add db

--- project/config/settings.py
+++ project/config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': "localhost",
+        'HOST': "db",
         'PORT': os.environ.get("DB_PORT"),
         # 'NAME': "postgres",
         # 'USER': "postgres",

07a70017c83fc1e42893dbc60db92e2574f3583d
Vaipik <n.khylko@gmail.com>
2023-02-12 14:01:44 +0200

    uncommented notes

--- project/apps/user_profile/services.py
+++ project/apps/user_profile/services.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from django.contrib.auth import get_user_model
 
 from apps.contacts.models import Contact
-# from apps.notes.models import Note
+from apps.notes.models import Note
 from apps.file_storage.models import File
 from .models import Profile
 
@@ -19,8 +19,8 @@ def get_last_five_files(user: User):
     return File.objects.select_related("category").filter(owner=user)[:5]
 
 
-# def get_last_five_notes(user: User):
-#     return Note.objects.filter(owner=user)[:5]
+def get_last_five_notes(user: User):
+    return Note.objects.filter(owner=user)[:5]
 
 
 def count_user_contacts(user: User):
@@ -31,8 +31,8 @@ def count_user_files(user: User):
     return File.objects.filter(owner=user).count()
 
 
-# def count_user_notes(user: User):
-#     return Note.objects.filter(owner=user).count()
+def count_user_notes(user: User):
+    return Note.objects.filter(owner=user).count()
 
 
 
@@ -40,17 +40,17 @@ def get_profile_context(user: User):
     """Accumulating context for user profile"""
     last_contacts = get_last_five_contacts(user)
     last_files = get_last_five_files(user)
-    # last_notes = get_last_five_notes(user)
+    last_notes = get_last_five_notes(user)
     contacts_amount = count_user_contacts(user)
     files_amount = count_user_files(user)
-    # notes_amount = count_user_notes(user)
+    notes_amount = count_user_notes(user)
     profile = Profile.objects.get(user=user)
     return {
         "contacts": last_contacts,
         "files": last_files,
-        # "notes": last_notes,
+        "notes": last_notes,
         "contacts_amount": contacts_amount,
         "files_amount": files_amount,
-        # "notes_amount": notes_amount,
+        "notes_amount": notes_amount,
         "profile": profile,
     }
